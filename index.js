0976747d30e835ef335ee2666acde1892185d1a3
omkar070402 <omkarseshadri007@gmail.com>
2025-03-26 16:37:36 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173", "https://to-do-app-frontend-murex-two.vercel.app/"],  // Add your frontend URL here
+    origin: ["http://localhost:5173", "https://to-do-app-frontend-murex-two.vercel.app"],  // Add your frontend URL here
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
 }));
